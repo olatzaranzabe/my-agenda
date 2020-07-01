27bfee60388f618b135d8f2b196189f554831f6c
olatzaranzabe <olatzaranzabe@gmail.com>
2020-07-01 19:40:29 +0200

    add s

--- src/features/ui/signup-page/signup-page.tsx
+++ src/features/ui/signup-page/signup-page.tsx
@@ -20,7 +20,7 @@ export const SignUpPage: React.FunctionComponent = () => {
   const [pasValue, setPasValue] = useState('');
   const [inputError, setInputError] = useState('');
   const history = useHistory();
-  const url = 'http://my-agenda-app.herokuapp.com/auth/signup';
+  const url = 'https://my-agenda-app.herokuapp.com/auth/signup';
 
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
